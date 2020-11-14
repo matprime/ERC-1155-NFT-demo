d3c5792663c190e7ef9dc7893cf0e96b6602d0d5
matprime <matt@mattprimeonline.com>
2020-11-14 16:56:22 +0100

    truffle test script

--- src/contracts/Memes.sol
+++ src/contracts/Memes.sol
@@ -5,9 +5,21 @@ pragma solidity ^0.6.0;
 import "./ERC1155.sol";
 
 contract Memes is ERC1155 {
-  uint256 public constant MEME = 0;
+  string[] public hashes;
+  mapping(string => bool) _hashExists;
 
   constructor() public ERC1155("") {
-    _mint(msg.sender, MEME, 1, "");
+  }
+
+  function mint(string memory _hash) public {
+    require(!_hashExists[_hash]);
+    hashes.push(_hash);
+    uint _id = hashes.length - 1;
+    _mint(msg.sender, _id, 1, "");
+    _hashExists[_hash] = true;
+  }
+
+  function getHashesCount() public view returns(uint count) {
+    return hashes.length;
   }
 }
--- test/Memes.test.js
+++ test/Memes.test.js
@@ -23,19 +23,18 @@ contract('Memes', (accounts) => {
   })
 
   describe('minting', async () => {
-
     it('creates a new NFT token', async () => {
       const result = await contract.mint('ECEA058EF4523')
-      const totalSupply = await contract.totalSupply()
-      // SUCCESS
+      const totalSupply = await contract.balanceOf(accounts[0], 0)
+      // is only one NFT of this type created in this account
       assert.equal(totalSupply, 1)
+      // check NFT id, from address, to address
       const event = result.logs[0].args
-      assert.equal(event.tokenId.toNumber(), 1, 'id is correct')
+      assert.equal(event.id, 0, 'id is correct')
       assert.equal(event.from, '0x0000000000000000000000000000000000000000', 'from is correct')
       assert.equal(event.to, accounts[0], 'to is correct')
-
       // FAILURE: cannot mint same hash twice
-      await contract.mint('ECEA058EF4523').should.be.rejected;
+      await contract.mint('ECEA058EF4523').should.be.rejected
     })
   })
 
@@ -45,12 +44,16 @@ contract('Memes', (accounts) => {
       await contract.mint('5386E4EABC345')
       await contract.mint('FFF567EAB5FFF')
       await contract.mint('234AEC00EFFD0')
-      const totalSupply = await contract.totalSupply()
 
+      //check number of minted NFTs
+      const memesCount = await contract.getHashesCount()
+      assert.equal(memesCount, 4)
+
+      //check hashes of minted NFTs
       let hash
       let result = []
 
-      for (var i = 1; i <= totalSupply; i++) {
+      for (var i = 1; i <= memesCount; i++) {
         hash = await contract.hashes(i - 1)
         result.push(hash)
       }
